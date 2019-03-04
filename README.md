707cef68aa351c802a2e9078516b2365080846f2
WILMER LEONEL ARTEAGA GAITAN <artewilm@colpatria.com>
2019-03-04 11:17:07 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Angular-AngularMaterial-Angularfire-NgRx" 
---
+++ test-angular/src/app/product/product.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  productName = 'Plane';
+  isDisabled = true;
+
+  constructor() { 
+    setTimeout(() => {
+      this.isDisabled = false;
+    }, 3000)
+  }
+
+  ngOnInit() {
+  }
+
+}
