af327e3e3fee2e2b01a450a5c82c94ab9b0c6b60
WILMER LEONEL ARTEAGA GAITAN <artewilm@colpatria.com>
2019-03-05 19:35:01 -0500

    Routing.

--- test-angular/src/app/app.module.ts
+++ test-angular/src/app/app.module.ts
@@ -6,16 +6,20 @@ import { ProductComponent } from './product/product.component';
 import { FormsModule } from "@angular/forms";
 import { ProductsComponent } from './products/products.component';
 import { ProductsService } from './products/products.service';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ProductsComponent
+    ProductsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [ProductsService],
   bootstrap: [AppComponent]
