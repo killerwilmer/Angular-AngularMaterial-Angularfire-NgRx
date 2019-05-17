a2a7013ec333f59b2ec8d4cbbccff1f9d08cc20c
Wilmer Arteaga <killerwilmer@gmail.com>
2019-05-16 22:06:03 -0500

    Dispatching Actions & Selectin State Slices

--- fitness-tracker/src/app/auth/auth.service.ts
+++ fitness-tracker/src/app/auth/auth.service.ts
@@ -7,7 +7,8 @@ import { TrainingService } from './../training/training.service';
 import { AuthData } from './auth-data.model';
 import { UIService } from '../shared/ui.service';
 import { Subject } from 'rxjs/Subject';
-import * as fromApp from '../app.reducer';
+import * as fromRoot from '../app.reducer';
+import * as UI from '../shared/ui.actions';
 
 @Injectable()
 export class AuthService {
@@ -19,7 +20,7 @@ export class AuthService {
     private afAuth: AngularFireAuth,
     private trainingService: TrainingService,
     private uiService: UIService,
-    private store: Store<{ui: fromApp.State}>
+    private store: Store<fromRoot.State>
   ) {}
 
   initAuthListener() {
@@ -39,32 +40,32 @@ export class AuthService {
 
   registerUser(authData: AuthData) {
     // this.uiService.loadingStateChanged.next(true);
-    this.store.dispatch({ type: 'START_LOADING'});
+    this.store.dispatch(new UI.StartLoading());
     this.afAuth.auth
       .createUserWithEmailAndPassword(authData.email, authData.password)
       .then(result => {
         // this.uiService.loadingStateChanged.next(false);
-        this.store.dispatch({ type: 'STOP_LOADING'});
+        this.store.dispatch(new UI.StopLoading());
       })
       .catch(error => {
         // this.uiService.loadingStateChanged.next(false);
-        this.store.dispatch({ type: 'STOP_LOADING'});
+        this.store.dispatch(new UI.StopLoading());
         this.uiService.showSnackbar(error.message, null, 3000);
       });
   }
 
   login(authData: AuthData) {
     // this.uiService.loadingStateChanged.next(true);
-    this.store.dispatch({ type: 'START_LOADING'});
+    this.store.dispatch(new UI.StartLoading());
     this.afAuth.auth
       .signInWithEmailAndPassword(authData.email, authData.password)
       .then(result => {
         // this.uiService.loadingStateChanged.next(false);
-        this.store.dispatch({ type: 'STOP_LOADING'});
+        this.store.dispatch(new UI.StopLoading());
       })
       .catch(error => {
         // this.uiService.loadingStateChanged.next(false);
-        this.store.dispatch({ type: 'STOP_LOADING'});
+        this.store.dispatch(new UI.StopLoading());
         this.uiService.showSnackbar(error.message, null, 3000);
       });
   }
--- fitness-tracker/src/app/auth/login/login.component.ts
+++ fitness-tracker/src/app/auth/login/login.component.ts
@@ -7,7 +7,7 @@ import { map } from 'rxjs/operators';
 
 import { UIService } from 'src/app/shared/ui.service';
 import { AuthService } from './../auth.service';
-import * as fromApp from '../../app.reducer';
+import * as fromRoot from '../../app.reducer';
 
 @Component({
   selector: 'app-login',
@@ -22,11 +22,11 @@ export class LoginComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private uiService: UIService,
-    private store: Store<{ ui: fromApp.State }>
+    private store: Store<fromRoot.State>
   ) {}
 
   ngOnInit() {
-    this.isLoading$ = this.store.pipe(map(state => state.ui.isLoading));
+    this.isLoading$ = this.store.select(fromRoot.getIsLoading);
     // this.loadingSubs = this.uiService.loadingStateChanged.subscribe(
     //   isLoading => {
     //     this.isLoading = isLoading;
