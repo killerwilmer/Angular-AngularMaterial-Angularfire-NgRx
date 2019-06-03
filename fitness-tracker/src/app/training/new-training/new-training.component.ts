b7dd62d808a86c39d5534ec5f7b1bc3a9829d55b
Wilmer Arteaga <killerwilmer@gmail.com>
2019-06-03 14:36:54 -0500

    Selecting Training State

--- fitness-tracker/src/app/training/new-training/new-training.component.ts
+++ fitness-tracker/src/app/training/new-training/new-training.component.ts
@@ -6,6 +6,7 @@ import { Store } from '@ngrx/store';
 import { TrainingService } from './../training.service';
 import { Exercise } from '../exercise.model';
 import { UIService } from 'src/app/shared/ui.service';
+import * as fromTraining from '../training.reducer';
 import * as fromRoot from '../../app.reducer';
 
 @Component({
@@ -13,24 +14,19 @@ import * as fromRoot from '../../app.reducer';
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.css']
 })
-export class NewTrainingComponent implements OnInit, OnDestroy {
-  exercises: Exercise[];
+export class NewTrainingComponent implements OnInit {
+  exercises$: Observable<Exercise[]>;
   isLoading$: Observable<boolean>;
-  private exercisesSubscription: Subscription;
 
   constructor(
     private trainingService: TrainingService,
     private uiService: UIService,
-    private store: Store<fromRoot.State>
+    private store: Store<fromTraining.State>
   ) {}
 
   ngOnInit() {
     this.isLoading$ = this.store.select(fromRoot.getIsLoading);
-    this.exercisesSubscription = this.trainingService.exercisesChanged.subscribe(
-      exercises => {
-        this.exercises = exercises;
-      }
-    );
+    this.exercises$ = this.store.select(fromTraining.getAvailableExercises);
     this.fetchExercises();
   }
 
@@ -40,11 +36,5 @@ export class NewTrainingComponent implements OnInit, OnDestroy {
 
   onStartTraining(form: NgForm) {
     this.trainingService.startExcercise(form.value.exercise);
-  }
-
-  ngOnDestroy(): void {
-    if (this.exercisesSubscription) {
-      this.exercisesSubscription.unsubscribe();
-    }
   }
 }
